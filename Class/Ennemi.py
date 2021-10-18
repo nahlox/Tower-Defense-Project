d9ec58526bba0f19f56665dfb411bfb460135763
GuilhemFrsh <frischguilhem@gmail.com>
2021-10-18 15:38:19 +0200

    test joueur

--- Class/Ennemi.py
+++ Class/Ennemi.py
@@ -1,12 +1,5 @@
-
-#from _typeshed import Self
-
-
 class Ennemi(object):
-
-
   def __init__(self, hp = 100, attack = 1, speed = 10, posX = 0, posY = 0, prize = 1):
-    
     self.hp = hp
     self.attack = attack
     self.speed = speed
@@ -16,37 +9,30 @@ class Ennemi(object):
     
 
   def set_Damage(self, damage = 1):
-   
     self.damage = damage
 
 
   def get_Hp(self):
-    
     return self.hp
 
 
   def get_Attack(self):
-   
     return self.attack
 
 
   def get_Speed(self):
-   
     return self.speed
 
 
   def get_PosX(self):
-   
     return self.posX
 
 
   def get_PosY(self):
-    
     return self.posY
 
 
   def get_Prize(self):
-    
     return self.prize
 
 
