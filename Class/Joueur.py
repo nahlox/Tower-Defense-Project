2b755327897786aaeaf64beda4fdf34cde328d83
GuilhemFrsh <frischguilhem@gmail.com>
2021-10-18 15:43:57 +0200

    test Projectile

--- Class/Joueur.py
+++ Class/Joueur.py
@@ -1,70 +1,47 @@
-# coding=System
+
 
 class Joueur(object):
 
   def __init__(self, money = 100, life = 30):
-
     self.money = money
     self.life = life
 
 
   def get_Money(self):
-
     return self.money
 
 
   def get_Life(self):
-
     return self.life
 
 
   def add_Money(self, prize = 0):
-
     self.money = self.money + prize
     return self.money
 
 
   def remove_Money(self, cost = 5):
-
     self.money = self.money - cost
     return self.money
 
 
   def add_Life(self,bonus, int = 0):
-
     self.life = self.life + bonus
 
 
   def remove_Life(self, damage = 1):
-    """
-     
-
-    @param int damage : 
-    @return  :
-    @author
-    """
     self.life = self.life - damage
 
 
   def create_tower(self, posX, posY):
-    """
-     
-
-    @param float posX : 
-    @param float posY : 
-    @return  :
-    @author
-    """
     pass
 
   def next_Ennemi(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     pass
 
+  def affiche(self):
+    print(self.money, self.life)
 
+joueur1 = Joueur() 
+joueur1.affiche()
 
--- Class/Projectile.py
+++ Class/Projectile.py
@@ -1,57 +1,6 @@
-# coding=System
-
 class Projectile(object):
 
-  """
-   
-
-  :version:
-  :author:
-  """
-
-  """ ATTRIBUTES
-
-   
-
-  velocity  (private)
-
-   
-
-  opacity  (private)
-
-   
-
-  posX  (private)
-
-   
-
-  posY  (private)
-
-   projectile direction :
-   y=Ax+B
-
-  PointA  (private)
-
-   projectile direction :
-   y=Ax+B
-
-  PointB  (private)
-
-  """
-
   def __init__(self, opacity = 0, posX = 0, posY = 0, pointA = 0, pointB = 0, velocity = 50):
-    """
-     
-
-    @param float opacity : 
-    @param float posX : 
-    @param float posY : 
-    @param float pointA : 
-    @param float pointB : 
-    @param int velocity : 
-    @return  :
-    @author
-    """
     self.opacity = opacity
     self.posX = posX
     self.posY = posY
@@ -61,75 +10,35 @@ class Projectile(object):
 
 
   def get_posX(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     return self.posX
 
 
   def get_posY(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     return self.posY
 
 
   def get_velocity(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     return self.velocity
 
 
   def set_direction(self, direction):
-    """
-     NOT CORRECT
-
-    @param float direction : 
-    @return  :
-    @author
-    """
     self.direction = direction
 
 
   def get_opacity(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     return self.opacity
 
 
   def get_pointA(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     return self.pointA
 
 
   def get_pointB(self):
-    """
-     
-
-    @return  :
-    @author
-    """
     return self.pointB
 
+  def affiche(self): 
+    print(self.posY, self.posX, self.pointA, self.pointB, self.opacity, self.velocity)
 
-
+projectile1 = Projectile()
+projectile1.affiche()
 
