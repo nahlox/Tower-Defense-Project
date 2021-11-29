99f2cff80d6d401e3b7f37e04b092919c4840ec0
Rawad Nahle <rawad.nahle10@gmail.com>
2021-11-29 15:44:24 +0100

    GUI-update (Unfinished)

--- GUI.py
+++ GUI.py
@@ -1,30 +1,77 @@
 import pygame
-from pygame.locals import *
+import sys
+
+
+# initializing the constructor
 pygame.init()
-width = 1000
-height = 500
-window = pygame.display.set_mode((width,height))
-bg_img = pygame.image.load('Assets/map.jpg')
-bg_img = pygame.transform.scale(bg_img,(width,height))
+
+# screen resolution
+res = (720,720)
+
+# opens up a window
+screen = pygame.display.set_mode(res)
+
+# white color
+color = (255,255,255)
+
+# light shade of the button
+color_light = (170,170,170)
+
+# dark shade of the button
+color_dark = (100,100,100)
+
+# stores the width of the
+# screen into a variable
+width = screen.get_width()
+
+# stores the height of the
+# screen into a variable
+height = screen.get_height()
+
+# defining a font
+smallfont = pygame.font.SysFont('Corbel',35)
+
+# rendering a text written in
+# this font
+quit = smallfont.render('QUIT' , True , color)
+play = smallfont.render('play' , True , color)
+
+while True:
+	
+	for ev in pygame.event.get():
+		
+		if ev.type == pygame.QUIT:
+			pygame.quit()
+			
+		#checks if a mouse is clicked
+		if ev.type == pygame.MOUSEBUTTONDOWN:
+			
+			#if the mouse is clicked on the
+			# button the game is terminated
+			if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+				pygame.quit()
+				
+	# fills the screen with a color
+	screen.fill((60,25,60))
+	
+	# stores the (x,y) coordinates into
+	# the variable as a tuple
+	mouse = pygame.mouse.get_pos()
+	
+	# if mouse is hovered on a button it
+	# changes to lighter shade
+	if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+		pygame.draw.rect(screen,color_light,[width/2,height/2,140,40])
+		
+	else:
+		pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40])
+	
+	# superimposing the text onto our button
+	screen.blit(quit , (width/2+50,height/2))
  
-
-
-while True :
-
-    window.fill((0,0,0))
-  
-    window.blit(bg_img, (0, 0))
-  
     
-    for event in pygame.event.get() :
-  
-        
-        if event.type == pygame.QUIT :
-  
-          
-            pygame.quit()
-  
-            quit()
-  
-          
-        pygame.display.update() 
+	
+	# updates the frames of the game
+	pygame.display.update()
+
+
